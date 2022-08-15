f4f6782ffb5d8ccba418635cf77df882fad048f2
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-15 16:31:26 -0300

    Connection with Database + connectiosn

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
 /* imports */
-const express = require('express');
-const mongoose = require('mongoose')
+require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // Rota Inicial
-app.get('/', (req, res) => {
-    res.send('Está funcionando!')
-})
-
+app.get("/", (req, res) => {
+  res.send("Está funcionando!");
+});
 
 // Rota de registro
-app.post('/register', (req, res) => {
+app.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password, confirmPassword, username } =
+    req.body;
 
-    const {email, password, confirmPassword, username} = req.body
-
-    if (!email){
-        res.status(422).json({ message: "O email é obrigatório!"})
-    }
-    if (!password) {
-        res.status(422).json({ message: "A senha é obrigatória"})
-    }
-    if (!confirmPassword) {
-        res.status(422).json({ message: "A confirmação de senha é obrigatória"})
-    }
-    if (password != confirmPassword) {
-        res.status(422).json({ message: "As senhas não correspondem a mesma!"})
-    }
-    if (!username) {
-        res.status(422).json({ message: "O nome de usuário é obrigatório"})
-    }
-})
+  if (!firstName) {
+    res.status(422).json({ message: "O primeiro nome é obrigatório!" });
+  }
+  if (!lastName) {
+    res.status(422).json({ message: "O último nome é obrigatório!" });
+  }
+  if (!email) {
+    res.status(422).json({ message: "O email é obrigatório!" });
+  }
+  if (!password) {
+    res.status(422).json({ message: "A senha é obrigatória" });
+  }
+  if (!confirmPassword) {
+    res.status(422).json({ message: "A confirmação de senha é obrigatória" });
+  }
+  if (password != confirmPassword) {
+    res.status(422).json({ message: "As senhas não correspondem a mesma!" });
+  }
+  if (!username) {
+    res.status(422).json({ message: "O nome de usuário é obrigatório" });
+  }
+});
 
 // Credencials
-const dbUser = process.env.USER
-const dbPassword = process.env.PASS
-const port = process.env.PORT
+const dbUser = process.env.USER;
+const dbPassword = process.env.PASS;
+const port = process.env.PORT;
 
 // Connection with Database
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.tkgejmb.mongodb.net/?retryWrites=true&w=majority`).then(port, () => {
-    app.listen(port)
-    console.log('Banco de dados conectado')
-})
+mongoose.connect(
+  `mongodb+srv://${dbUser}:${dbPassword}@cluster0.2dr1nh7.mongodb.net/?retryWrites=true&w=majority`
+).then(() => {
+  app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
+  console.log('Banco de dados conectado!')
+}).catch(error => console.log('Erro: ' + error))
