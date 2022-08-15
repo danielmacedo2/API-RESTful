d19c5b19484800683149959bfd3a194816570465
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-15 15:05:19 -0300

    Criando rota de registro

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
     res.send('Está funcionando!')
 })
 
+app.post('/register', (req, res) => {
+
+    const {username, password, confirmPassword} = req.body
+
+})
+
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
