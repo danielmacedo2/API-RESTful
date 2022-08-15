ec81baf35e57a3cfff49415e2ef6ce4e49ed9826
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-15 15:19:42 -0300

    Validations append

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* imports */
 const express = require('express');
 const mongoose = require('mongoose')
 
@@ -5,13 +6,33 @@ const app = express();
 
 app.use(express.json())
 
+// Rota Inicial
 app.get('/', (req, res) => {
     res.send('Está funcionando!')
 })
 
+
+// Rota de registro
 app.post('/register', (req, res) => {
 
     const {email, password, confirmPassword, username} = req.body
+
+    if (!email){
+        res.status(422).json({ message: "O email é obrigatório!"})
+    }
+    if (!password) {
+        res.status(422).json({ message: "A senha é obrigatória"})
+    }
+    if (!confirmPassword) {
+        res.status(422).json({ message: "A confirmação de senha é obrigatória"})
+    }
+    if (password != confirmPassword) {
+        res.status(422).json({ message: "As senhas não correspondem a mesma!"})
+    }
+    if (!username) {
+        res.status(422).json({ message: "O nome de usuário é obrigatório"})
+    }
+    
 })
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
