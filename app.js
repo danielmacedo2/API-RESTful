6230335b113233f3ecc2472da76ba4430f2a649e
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-16 11:49:15 -0300

    Checking if username is already in use - Validation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* imports */
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 // Modelo de Usuário
-const User = require('./Models/User')
+const User = require("./Models/User");
 
 // Rota de registro
 app.post("/auth/register", async (req, res) => {
@@ -33,22 +33,35 @@ app.post("/auth/register", async (req, res) => {
     return res.status(422).json({ message: "A senha é obrigatória" });
   }
   if (!confirmPassword) {
-    return res.status(422).json({ message: "A confirmação de senha é obrigatória" });
+    return res
+      .status(422)
+      .json({ message: "A confirmação de senha é obrigatória" });
   }
-  if (password != confirmPassword) {
-    return res.status(422).json({ message: "As senhas não correspondem a mesma!" });
+  if (password !== confirmPassword) {
+    return res
+      .status(422)
+      .json({ message: "As senhas não correspondem a mesma!" });
   }
   if (!username) {
     return res.status(422).json({ message: "O nome de usuário é obrigatório" });
   }
 
-  const userExist = await User.findOne({ email: email })
+  const userExist = await User.findOne({ email: email });
 
-  if(userExist) {
-    return res.status(422).json({ message: "Esse email já esta em uso, por favor utilize outro e-mail!" })
+  if (userExist) {
+    return res
+    .status(422)
+      .json({
+        message: "Esse email já esta em uso, por favor utilize outro e-mail!",
+      });
+  }
+  
+  const usernameIsAlreadyInUse = await User.findOne({ username: username });
+
+  if(usernameIsAlreadyInUse) {
+    return res.status(422).json({ message: "nome de usuário já em uso por favor tente outro"})
   }
 
-  const usernameAlreadyInUse = await User.findOne({ username: username })
 });
 
 // Credencials
@@ -57,9 +70,12 @@ const dbPassword = process.env.PASS;
 const port = process.env.PORT;
 
 // Connection with Database
-mongoose.connect(
-  `mongodb+srv://${dbUser}:${dbPassword}@cluster0.2dr1nh7.mongodb.net/?retryWrites=true&w=majority`
-).then(() => {
-  app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
-  console.log('Banco de dados conectado!')
-}).catch(error => console.log('Erro: ' + error))
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.2dr1nh7.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+    console.log("Banco de dados conectado!");
+  })
+  .catch((error) => console.log("Erro: " + error));
