9615a651bfeabe7323d2a047ac97e2e9e8f60c6c
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-16 08:25:22 -0300

    Modelo de usuário criado

---
+++ Models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User', {
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: {
+        type: String,
+        minlength: 6
+    }
+})
